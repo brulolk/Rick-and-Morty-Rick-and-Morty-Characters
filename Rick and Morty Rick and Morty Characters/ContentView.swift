60da7c7a24fe07febfaf0f60726d3e8bd777ddc3
Bruno Vinicius Siqueira Santos <bruno10916@gmail.com>
2026-03-20 18:39:55 -0300

    Initial Commit

---
+++ Rick and Morty Rick and Morty Characters/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rick and Morty Rick and Morty Characters
+//
+//  Created by Bruno Vinicius on 20/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rick and Morty Rick and Morty Characters/Rick_and_Morty_Rick_and_Morty_CharactersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rick_and_Morty_Rick_and_Morty_CharactersApp.swift
+//  Rick and Morty Rick and Morty Characters
+//
+//  Created by Bruno Vinicius on 20/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Rick_and_Morty_Rick_and_Morty_CharactersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
